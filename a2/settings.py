9ef96951d824a78ce1afdc06828ef3ecb3dc17bb
100101101 <rikhiboy@gmail.com>
2024-08-16 16:50:58 +0800

    settings.py

--- a2/settings.py
+++ a2/settings.py
@@ -22,16 +22,16 @@ DEFAULT_DETECT_IMAGE = DEFAULT_IMAGES_DIR / os.getenv('DEFAULT_DETECT_IMAGE', 'y
 # Videos config (default, can be overridden by environment variables)
 DEFAULT_VIDEO_DIR = Path(os.getenv('VIDEO_DIR', ROOT / 'videos'))
 
-# Directly referencing the specific video in Downloads
-SPECIFIC_VIDEO_PATH = Path(os.getenv('SPECIFIC_VIDEO', r"C:\Users\User\Downloads\WhatsApp Video 2023-03-18 at 1.43.01 AM.mp4"))
+# # Directly referencing the specific video in Downloads
+# SPECIFIC_VIDEO_PATH = Path(os.getenv('SPECIFIC_VIDEO', r"C:\Users\User\Downloads\WhatsApp Video 2023-03-18 at 1.43.01 AM.mp4"))
 
-# Dictionary to easily reference videos
-VIDEOS_DICT = {
-    'specific_video': SPECIFIC_VIDEO_PATH,
-}
+# # Dictionary to easily reference videos
+# VIDEOS_DICT = {
+#     'specific_video': SPECIFIC_VIDEO_PATH,
+# }
 
 # ML Model config (default, can be overridden by environment variables)
 MODEL_DIR = Path(os.getenv('MODEL_DIR', ROOT / 'weights'))
 PRETRAINED_MODEL = MODEL_DIR / Path(os.getenv('PRETRAINED_MODEL', 'yolov8n.pt'))
-CUSTOM_MODEL = Path(r"C:\Users\User\ImgProcessing\Assignment2\runs\detect\yolov8_electronic_waste9\weights\best.pt")
+CUSTOM_MODEL = Path(r"C:\Users\User\ImgProcessing\Assignment2\CompVisionAssignment\a2\yolov8_electronic_waste9\weights\best.pt")
 SEGMENTATION_MODEL = MODEL_DIR / os.getenv('SEGMENTATION_MODEL', 'yolov8n-seg.pt')
